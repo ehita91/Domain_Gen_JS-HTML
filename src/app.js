e0d980d764d52494a086a8e0fb958a77dbe46195
Ernesto Hita <ehita91@gmail.com>
2023-02-13 08:04:25 +0000

    Generador de Domninios aleatorios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let name = ["google", "adobe", "nvidia", "apple", "samsung", "nike"];
+  let country = [".com", ".net", ".cu", ".uk", ".it", ".org"];
+
+  function generateDom() {
+    document.getElementById("dom").innerHTML =
+      name[Math.floor(Math.random() * name.length)] +
+      country[Math.floor(Math.random() * country.length)];
+  }
+  document.getElementById("btn").addEventListener("click", generateDom);
+
   console.log("Hello Rigo from the console!");
 };
